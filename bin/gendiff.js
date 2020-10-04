79cf51b41831c882f59c1e6b2d063ce5ed3aae3a
Dmitriy Gerasimov <up@websector.ru>
2020-10-04 17:03:58 +0300

    fix typo

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,5 @@ program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format');
 program.parse(process.argv);

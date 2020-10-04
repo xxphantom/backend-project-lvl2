5998b32c088602076d95d54f9a4b606187397d44
Dmitriy Gerasimov <up@websector.ru>
2020-10-04 15:58:44 +0300

    add option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,6 @@ const version = '1.0.0';
 program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
-
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 program.parse(process.argv);

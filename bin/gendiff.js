0a53fbdd2aa3553e85d4eac4e124c71889da068d
Dmitriy Gerasimov <up@websector.ru>
2020-10-09 12:18:01 +0300

    rename some const and func

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ import process from 'process';
 import diff from '../src/index.js';
 
 const version = '1.0.0';
-const convertToAbsPath = (relFilepath) => path.resolve(process.cwd(), relFilepath);
+const fullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 program
   .version(version)
@@ -14,6 +14,6 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(diff(convertToAbsPath(filepath1), convertToAbsPath(filepath2)));
+    console.log(diff(fullPath(filepath1), fullPath(filepath2)));
   });
 program.parse(process.argv);

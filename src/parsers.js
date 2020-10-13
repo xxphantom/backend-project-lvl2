e29777c5d73e83eadf301e11159f91fef8c5b2ab
Dmitriy Gerasimov <up@websector.ru>
2020-10-13 23:15:41 +0300

    rename some const

--- src/parsers.js
+++ src/parsers.js
@@ -1,17 +1,16 @@
 import fs from 'fs';
 import path from 'path';
-
 import yaml from 'js-yaml';
 
-const type = {
+const parsers = {
   '.yml': (yamlData) => yaml.safeLoad(yamlData),
   '.json': (jsonData) => JSON.parse(jsonData),
 };
 
 const parse = (filepath) => {
   const rawData = fs.readFileSync(filepath, 'utf8');
-  const extname = path.extname(filepath);
-  const data = type[extname](rawData);
+  const extname = path.extname(filepath) || '.json';
+  const data = parsers[extname](rawData);
   return data;
 };
 export default parse;

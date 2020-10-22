7d8058714a3ec0b3c3750ae38b97ae10b22e0785
Dmitriy Gerasimov <up@websector.ru>
2020-10-22 16:05:09 +0300

    split parser and file reader and rename nested nodes to node

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import _ from 'lodash';
 import parse from './parsers.js';
 import getFormatter from './formatters/index.js';
+import dataLoader from './dataLoader.js';
 
 const diff = (filepath1, filepath2, format = 'stylish') => {
-  const dataBefore = parse(filepath1);
-  const dataAfter = parse(filepath2);
   const formatter = getFormatter(format);
+  const [rawData1, fileFormat1] = dataLoader(filepath1);
+  const [rawData2, fileFormat2] = dataLoader(filepath2);
+  const parsedData1 = parse(rawData1, fileFormat1);
+  const parsedData2 = parse(rawData2, fileFormat2);
 
   const iter = (data1, data2) => {
-    const uniqueKeys = _.union(_.keys(data2), _.keys(data1)).sort();
+    const uniqueKeys = _.union(_.keys(data2), _.keys(data1));
+    const nestedKeys = uniqueKeys
+      .filter((key) => (_.isObject(data1[key]) && _.isObject(data2[key])));
     const deletedKeys = _.difference(uniqueKeys, _.keys(data2));
     const addedKeys = _.difference(uniqueKeys, _.keys(data1));
-    const nodesKeys = _.difference(uniqueKeys, deletedKeys, addedKeys)
-      .filter((key) => (_.isObject(data1[key]) && _.isObject(data2[key])));
-    const changedKeys = _.difference(uniqueKeys, deletedKeys, addedKeys, nodesKeys)
+    const changedKeys = _.difference(uniqueKeys, deletedKeys, addedKeys, nestedKeys)
       .filter((key) => data1[key] !== data2[key]);
-    const unchangedKeys = _.difference(uniqueKeys, deletedKeys, addedKeys, nodesKeys)
-      .filter((key) => data1[key] === data2[key]);
+    const unchangedKeys = _.difference(uniqueKeys, nestedKeys, deletedKeys, addedKeys, changedKeys);
 
-    const deletedProps = deletedKeys.map((key) => [key, 'deleted', data1[key]]);
-    const addedProps = addedKeys.map((key) => [key, 'added', data2[key]]);
-    const changedProps = changedKeys.map((key) => [key, 'changed', data1[key], data2[key]]);
-    const unchangedProps = unchangedKeys.map((key) => [key, 'unchanged', data1[key]]);
-    const nodesProps = nodesKeys.map((key) => [key, 'node', iter(data1[key], data2[key])]);
-    return [...deletedProps, ...addedProps, ...changedProps, ...unchangedProps, ...nodesProps]
-      .sort();
+    const deleted = deletedKeys.map((key) => [key, 'deleted', data1[key]]);
+    const added = addedKeys.map((key) => [key, 'added', data2[key]]);
+    const changed = changedKeys.map((key) => [key, 'changed', data1[key], data2[key]]);
+    const unchanged = unchangedKeys.map((key) => [key, 'unchanged', data1[key]]);
+    const nested = nestedKeys.map((key) => [key, 'node', iter(data1[key], data2[key])]);
+    return [...deleted, ...added, ...changed, ...unchanged, ...nested].sort();
   };
-  const resultAstTree = iter(dataBefore, dataAfter);
+  const resultAstTree = iter(parsedData1, parsedData2);
   return formatter ? formatter(resultAstTree) : `error: incorrect format: ${format}`;
 };
 
